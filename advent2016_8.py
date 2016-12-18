7abace64b8a32076f5a64f42718d9d1f1716bed7
kbaikov <konstantin.baikov@gmail.com>
2016-12-18 21:58:03 +0100

    Solve advent 8 part 1

--- advent2016_8.py
+++ advent2016_8.py
@@ -4,11 +4,11 @@
 from collections import deque
 import logging as log
 from pprint import pprint
-log.basicConfig(level=log.DEBUG, 
+log.basicConfig(level=log.DEBUG,
                 format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-grid = {(c, r): None for c in range(7) for r in range(3)}
+grid = {(c, r): None for c in range(50) for r in range(6)}
 
 
 def rect(x, y):
@@ -18,38 +18,63 @@ def rect(x, y):
 
 
 def rotate_column(column, rotation):
-    d = deque([grid[column, y] for y in range(3)])
+    d = deque([grid[column, y] for y in range(6)])
     d.rotate(rotation)
     for i, x in enumerate(d):
         grid[column, i] = x
 
 
 def rotate_row(row, rotation):
-    d = deque([grid[x, row] for x in range(7)])
+    d = deque([grid[x, row] for x in range(50)])
     d.rotate(rotation)
     for i, x in enumerate(d):
         grid[i, row] = x
 
 
 def print_grid():
-    for y in range(3):
-        for x in range(7):
+    for y in range(6):
+        for x in range(50):
             print(grid[x, y], end='\t')
         print()
 
 
+def parse_commands(line):
+    function = None
+    parameter1 = None
+    parameter2 = None
+    if line.startswith("rect"):
+        function = 'rect'
+        parameter1, _, parameter2 = line.rpartition('x')
+    elif line.startswith("rotate row"):
+        function = 'rotate_row'
+        parameter1, _, parameter2 = line.rpartition(' by ')
+    else:
+        function = 'rotate_column'
+        parameter1, _, parameter2 = line.rpartition(' by ')
+
+    if parameter1[-2:].isdigit():
+        parameter1 = parameter1[-2:]
+    else:
+        parameter1 = parameter1[-1]
+    return function, parameter1, parameter2
+
 if __name__ == '__main__':
-    rect(3, 2)
-    print_grid()
-    rotate_column(1, 1)
-    print_grid()
-    rotate_row(0, 4)
-    print_grid()
-    rotate_column(1, 1)
-    print_grid()
+    # parse_commands('rotate row y=0 by 5')
+    # rect(3, 2)
+    # print_grid()
+    # rotate_column(1, 1)
+    # print_grid()
+    # rotate_row(0, 4)
+    # print_grid()
+    # rotate_column(1, 1)
+    # print_grid()
     # log.debug(grid[0, 0])
-    # with open('advent2016-8_input.txt') as file:
-    #     letters = [[] for _ in range(8)]
-    #     for line in file:
-    #         line = line.rstrip('\r\n')
-            
+    with open('advent2016-8_input.txt') as file:
+        letters = [[] for _ in range(8)]
+        for line in file:
+            line = line.rstrip('\r\n')
+            f, p1, p2 = parse_commands(line)
+            locals()[f](int(p1), int(p2))
+    print_grid()
+    print(list(grid.values()).count('x'))
+
