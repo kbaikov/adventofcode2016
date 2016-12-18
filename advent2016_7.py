3caa6bf2a938fab9dec10b9fbe492246eb8337a6
kbaikov <konstantin.baikov@gmail.com>
2016-12-18 23:41:55 +0100

    Pass advent 7 tests
    But the result is not correct because of double brackets

--- advent2016_7.py
+++ advent2016_7.py
@@ -6,12 +6,39 @@ import logging as log
 log.basicConfig(level=log.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def supports_tls(string):
-    pass
+def supports_tls(s):
+    if has_abba(s)[0]:
+        if not abba_inside_square_brackets(s, has_abba(s)[1]):
+            return True
+    return False
+
+
+
+def has_abba(s):
+    index = 0
+    while index + 3 < len(s):
+        if s[index] == s[index + 1]:
+            index += 1
+            continue
+        if s[index] == s[index + 3] and s[index + 1] == s[index + 2]:
+            return True, s[index:index+4]
+        index += 1
+    return False, None
+
+
+def abba_inside_square_brackets(s, abba):
+    index = s.find(abba)
+    if s[index - 1] == "[" and s[index + 4] == "]":
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
+    results = []
     with open('advent2016-7_input.txt') as file:
-        letters = [[] for _ in range(8)]
         for line in file:
             line = line.rstrip('\r\n')
-            
+            results.append(supports_tls(line))
+            print(results.count(True))
+
+
--- test_advent2016_7.py
+++ test_advent2016_7.py
@@ -1,6 +1,6 @@
 import pytest
 
-from advent2016_7 import supports_tls
+from advent2016_7 import supports_tls, has_abba, abba_inside_square_brackets
 
 
 @pytest.mark.parametrize("string, expected", [
@@ -11,3 +11,23 @@ from advent2016_7 import supports_tls
 ])
 def test_supports_tls(string, expected):
     assert supports_tls(string) == expected
+
+
+@pytest.mark.parametrize("string, expected", [
+    ("abba", (True, 'abba')),
+    ("aaaa", (False, None)),
+    ("aaba", (False, None)),
+    ("ioxxoj", (True, 'oxxo')),
+])
+def test_has_abba(string, expected):
+    assert has_abba(string) == expected
+
+
+@pytest.mark.parametrize("string, match, expected", [
+    ("[abba]", 'abba', True),
+    ("[i]oxxoj", 'oxxo', False),
+    ("ioxxo[j]", 'oxxo', False),
+    ("[oxxo]", 'oxxo', True),
+])
+def test_abba_inside_square_brackets(string, match, expected):
+    assert abba_inside_square_brackets(string, match) == expected
