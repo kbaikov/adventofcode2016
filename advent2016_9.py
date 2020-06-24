52ff9aefb00aadb7ac588934815177f3c67bd3ea
kbaikov <konstantin.baikov@gmail.com>
2020-06-24 20:46:02 +0200

    Solve day 9 part 2

--- advent2016_9.py
+++ advent2016_9.py
@@ -15,16 +15,32 @@ def unzipped(s):
             stack.append(s[current_position:])
             break
         start, end = marker.span()
-        length, _, multiplier = marker.group().strip("()").partition("x")
-        length = int(length)
-        multiplier = int(multiplier)
+        length, multiplier = int(marker.group(1)), int(marker.group(2))
         stack.append(s[current_position:start])
         stack.append(s[end : end + length] * multiplier)
         current_position = end + length
     return "".join(stack)
 
 
-if __name__ == "__main__":
+def unzipped2(s):
+    """from https://www.reddit.com/r/adventofcode/comments/5hbygy/2016_day_9_solutions/daz2o0d/"""
+    marker = re.search(r"\((\d+)x(\d+)\)", s)
+    if not marker:
+        return len(s)
 
+    current_position = marker.start(0)
+    length, multiplier = int(marker.group(1)), int(marker.group(2))
+
+    i = current_position + len(marker.group())
+    return (
+        len(s[:current_position])
+        + unzipped2(s[i : i + length]) * multiplier
+        + unzipped2(s[i + length :])
+    )
+
+
+if __name__ == "__main__":
     with open("advent2016-9_input.txt") as file:
-        print(len(unzipped(file.read().strip())))  # 107035
+        text = file.read().strip()
+        print(len(unzipped(text)))  # 107035
+        print(unzipped2(text))  # 11451628995
--- tests/test_advent2016_9.py
+++ tests/test_advent2016_9.py
@@ -16,3 +16,18 @@ from advent2016_9 import *
 )
 def test_unzipped(string, expected):
     assert unzipped(string) == expected
+
+
+@pytest.mark.parametrize(
+    "string, expected",
+    [
+        ("ADVENT", 6),
+        ("A(1x5)BC", 7),
+        ("(3x3)XYZ", 9),
+        ("X(8x2)(3x3)ABCY", 20),
+        ("(27x12)(20x12)(13x14)(7x10)(1x12)A", 241920),
+        ("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", 445),
+    ],
+)
+def test_unzipped2(string, expected):
+    assert unzipped2(string) == expected
